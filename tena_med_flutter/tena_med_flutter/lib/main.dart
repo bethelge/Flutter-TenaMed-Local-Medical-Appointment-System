f7df6607e6d019a58f44fc2d8dac084f95ea4c00
Bethel Gelan <bethelgelan16@gmail.com>
2025-06-11 23:17:39 +0300

    edited main.dart

--- tena_med_flutter/tena_med_flutter/lib/main.dart
+++ tena_med_flutter/tena_med_flutter/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tena_med_flutter/features/appointment/presentation/screens/welcome_screen.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/doctor_profile_after_setup_screen.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/doctor_profile_setup_screen.dart';
 
+import 'features/appointment/data/models/doctor_card.dart';
 import 'features/appointment/presentation/screens/PatientLoginScreen.dart';
 import 'features/appointment/presentation/screens/appointment_list_screen.dart';
 import 'features/appointment/presentation/screens/book_appointment_screen.dart';
@@ -10,8 +13,6 @@ import 'features/appointment/presentation/screens/booking_succsess.dart';
 import 'features/appointment/presentation/screens/doctor_appointment_detail.dart';
 import 'features/appointment/presentation/screens/doctor_home_screen_after_setup.dart';
 import 'features/appointment/presentation/screens/doctor_login_screen.dart';
-import 'features/appointment/presentation/screens/doctor_profile_after_setup_screen.dart';
-import 'features/appointment/presentation/screens/doctor_profile_setup_screen.dart';
 import 'features/appointment/presentation/screens/doctor_signup_screen.dart';
 import 'features/appointment/presentation/screens/patient_landing_screen.dart';
 import 'features/appointment/presentation/screens/patient_signup_screen.dart';
@@ -19,8 +20,7 @@ import 'features/appointment/presentation/screens/post_booking_home_screen.dart'
 import 'features/appointment/presentation/screens/privacy_policy_screen.dart';
 import 'features/appointment/presentation/screens/profile_screen.dart';
 import 'features/appointment/presentation/screens/settings_screen.dart';
-
-void main() {
+import 'features/appointment/presentation/widgets/DoctorDetailScreen.dart';void main() {
   runApp(const ProviderScope(child: TenaMedApp()));
 }
 
@@ -36,7 +36,32 @@ class TenaMedApp extends StatelessWidget {
           path: '/',
           builder: (context, state) => const WelcomeScreen(),
         ),
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const DoctorLoginScreen(),
+        ),
 
+        GoRoute(
+          path: '/doctor_profile_after_setup',
+          builder: (context, state) => const DoctorProfileAfterSetupScreen(),
+        ),
+
+        GoRoute(
+          path: '/signup',
+          builder: (context, state) => const DoctorSignupScreen(),
+        ),
+        GoRoute(
+          path: '/doctor_profile_setup',
+          builder: (context, state) => const DoctorProfileSetupScreen(),
+        ),
+        GoRoute(
+          path: '/doctor_home_after_setup',
+          builder: (context, state) => const DoctorHomeScreenAfterSetup(),
+        ),
+        GoRoute(
+          path: '/doctor_appointment_detail',
+          builder: (context, state) => const DoctorAppointmentDetailScreen(),
+        ),
         GoRoute(
           path: '/patient-login',
           builder: (context, state) => const PatientLoginScreen(),
@@ -46,52 +71,16 @@ class TenaMedApp extends StatelessWidget {
           builder: (context, state) => const PatientSignupScreen(),
         ),
         GoRoute(
-          path: '/login',
-          builder: (context, state) => const DoctorLoginScreen(),
-        ),
-        GoRoute(
-          path: '/signup',
-          builder: (context, state) => const DoctorSignupScreen(),
-        ),
-        GoRoute(
           path: '/post-booking-home',
           builder: (context, state) => const PostBookingHomeScreen(),
-        ),
-        GoRoute(
-          path: '/doctor_profile_setup',
-          builder: (context, state) => const DoctorProfileSetupScreen(),
-        ),
-        GoRoute(
-          path: '/doctor_profile_after_setup',
-          builder: (context, state) => const DoctorProfileAfterSetupScreen(),
-        ),
-        GoRoute(
-          path: '/patient-landing',
-          builder: (context, state) =>  PatientLandingScreen(),
         ),
         GoRoute(
           path: '/appointment-list',
           builder: (context, state) => const AppointmentListScreen(),
         ),
         GoRoute(
-          path: '/book_appointment',
-          builder: (context, state) => const BookAppointmentScreen(),
-        ),
-        GoRoute(
-          path: '/success_screen',
-          builder: (context, state) => const BookingSuccessScreen(),
-        ),
-        GoRoute(
-          path: '/doctor_appointment_detail',
-          builder: (context, state) => const DoctorAppointmentDetailScreen(),
-        ),
-        GoRoute(
-          path: '/doctor_home_after_setup',
-          builder: (context, state) => const DoctorHomeScreenAfterSetup(),
-        ),
-        GoRoute(
-          path: '/privacy-policy',
-          builder: (context, state) => const PrivacyPolicyScreen(),
+          path: '/patient-landing',
+          builder: (context, state) =>  PatientLandingScreen(),
         ),
         GoRoute(
           path: '/patient_profile',
@@ -100,6 +89,328 @@ class TenaMedApp extends StatelessWidget {
         GoRoute(
           path: '/settings',
           builder: (context, state) => const SettingsScreen(),
+        ),
+        GoRoute(
+          path: '/privacy-policy',
+          builder: (context, state) => const PrivacyPolicyScreen(),
+        ),
+        GoRoute(
+          path: '/doctor-detail',
+          builder: (context, state) {
+            final doctor = state.extra as DoctorCard;
+
+            return DoctorDetailScreen(
+              name: doctor.name,
+              specialization: doctor.specialization,
+              imagePath: doctor.imagePath,
+              biography: doctor.biography,
+            );
+          },
+        ),
+
+        GoRoute(
+          path: '/book_appointment',
+          builder: (context, state) => const BookAppointmentScreen(),
+        ),
+        GoRoute(
+          path: '/success_screen',
+          builder: (context, state) => const BookingSuccessScreen(),
+        ),
+
+      ],
+    );
+
+
+    return MaterialApp.router(
+      title: 'TenaMed',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+      ),
+      routerConfig: _router,
+
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TenaMed Home')),
+      body: const Center(
+        child: Text('Welcome to TenaMed 🚑'),
+      ),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/welcome_screen.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/doctor_profile_after_setup_screen.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/doctor_profile_setup_screen.dart';
+
+import 'features/appointment/data/models/doctor_card.dart';
+import 'features/appointment/presentation/screens/PatientLoginScreen.dart';
+import 'features/appointment/presentation/screens/appointment_list_screen.dart';
+import 'features/appointment/presentation/screens/book_appointment_screen.dart';
+import 'features/appointment/presentation/screens/booking_succsess.dart';
+import 'features/appointment/presentation/screens/doctor_appointment_detail.dart';
+import 'features/appointment/presentation/screens/doctor_home_screen_after_setup.dart';
+import 'features/appointment/presentation/screens/doctor_login_screen.dart';
+import 'features/appointment/presentation/screens/doctor_signup_screen.dart';
+import 'features/appointment/presentation/screens/patient_landing_screen.dart';
+import 'features/appointment/presentation/screens/patient_signup_screen.dart';
+import 'features/appointment/presentation/screens/post_booking_home_screen.dart';
+import 'features/appointment/presentation/screens/privacy_policy_screen.dart';
+import 'features/appointment/presentation/screens/profile_screen.dart';
+import 'features/appointment/presentation/screens/settings_screen.dart';
+import 'features/appointment/presentation/widgets/DoctorDetailScreen.dart';void main() {
+  runApp(const ProviderScope(child: TenaMedApp()));
+}
+
+class TenaMedApp extends StatelessWidget {
+  const TenaMedApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _router = GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const WelcomeScreen(),
+        ),
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const DoctorLoginScreen(),
+        ),
+
+        GoRoute(
+          path: '/doctor_profile_after_setup',
+          builder: (context, state) => const DoctorProfileAfterSetupScreen(),
+        ),
+
+        GoRoute(
+          path: '/signup',
+          builder: (context, state) => const DoctorSignupScreen(),
+        ),
+        GoRoute(
+          path: '/doctor_profile_setup',
+          builder: (context, state) => const DoctorProfileSetupScreen(),
+        ),
+        GoRoute(
+          path: '/doctor_home_after_setup',
+          builder: (context, state) => const DoctorHomeScreenAfterSetup(),
+        ),
+        GoRoute(
+          path: '/doctor_appointment_detail',
+          builder: (context, state) => const DoctorAppointmentDetailScreen(),
+        ),
+        GoRoute(
+          path: '/patient-login',
+          builder: (context, state) => const PatientLoginScreen(),
+        ),
+        GoRoute(
+          path: '/patient-signup',
+          builder: (context, state) => const PatientSignupScreen(),
+        ),
+        GoRoute(
+          path: '/post-booking-home',
+          builder: (context, state) => const PostBookingHomeScreen(),
+        ),
+        GoRoute(
+          path: '/appointment-list',
+          builder: (context, state) => const AppointmentListScreen(),
+        ),
+        GoRoute(
+          path: '/patient-landing',
+          builder: (context, state) =>  PatientLandingScreen(),
+        ),
+        GoRoute(
+          path: '/patient_profile',
+          builder: (context, state) => ProfileScreen(onLogout: () {  },),
+        ),
+        GoRoute(
+          path: '/settings',
+          builder: (context, state) => const SettingsScreen(),
+        ),
+        GoRoute(
+          path: '/privacy-policy',
+          builder: (context, state) => const PrivacyPolicyScreen(),
+        ),
+        GoRoute(
+          path: '/doctor-detail',
+          builder: (context, state) {
+            final doctor = state.extra as DoctorCard;
+
+            return DoctorDetailScreen(
+              name: doctor.name,
+              specialization: doctor.specialization,
+              imagePath: doctor.imagePath,
+              biography: doctor.biography,
+            );
+          },
+        ),
+
+        GoRoute(
+          path: '/book_appointment',
+          builder: (context, state) => const BookAppointmentScreen(),
+        ),
+        GoRoute(
+          path: '/success_screen',
+          builder: (context, state) => const BookingSuccessScreen(),
+        ),
+
+      ],
+    );
+
+
+    return MaterialApp.router(
+      title: 'TenaMed',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+      ),
+      routerConfig: _router,
+
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TenaMed Home')),
+      body: const Center(
+        child: Text('Welcome to TenaMed 🚑'),
+      ),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/welcome_screen.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/doctor_profile_after_setup_screen.dart';
+import 'package:tena_med_flutter/features/appointment/presentation/screens/doctor_profile_setup_screen.dart';
+
+import 'features/appointment/data/models/doctor_card.dart';
+import 'features/appointment/presentation/screens/PatientLoginScreen.dart';
+import 'features/appointment/presentation/screens/appointment_list_screen.dart';
+import 'features/appointment/presentation/screens/book_appointment_screen.dart';
+import 'features/appointment/presentation/screens/booking_succsess.dart';
+import 'features/appointment/presentation/screens/doctor_appointment_detail.dart';
+import 'features/appointment/presentation/screens/doctor_home_screen_after_setup.dart';
+import 'features/appointment/presentation/screens/doctor_login_screen.dart';
+import 'features/appointment/presentation/screens/doctor_signup_screen.dart';
+import 'features/appointment/presentation/screens/patient_landing_screen.dart';
+import 'features/appointment/presentation/screens/patient_signup_screen.dart';
+import 'features/appointment/presentation/screens/post_booking_home_screen.dart';
+import 'features/appointment/presentation/screens/privacy_policy_screen.dart';
+import 'features/appointment/presentation/screens/profile_screen.dart';
+import 'features/appointment/presentation/screens/settings_screen.dart';
+import 'features/appointment/presentation/widgets/DoctorDetailScreen.dart';void main() {
+  runApp(const ProviderScope(child: TenaMedApp()));
+}
+
+class TenaMedApp extends StatelessWidget {
+  const TenaMedApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _router = GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const WelcomeScreen(),
+        ),
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const DoctorLoginScreen(),
+        ),
+
+        GoRoute(
+          path: '/doctor_profile_after_setup',
+          builder: (context, state) => const DoctorProfileAfterSetupScreen(),
+        ),
+
+        GoRoute(
+          path: '/signup',
+          builder: (context, state) => const DoctorSignupScreen(),
+        ),
+        GoRoute(
+          path: '/doctor_profile_setup',
+          builder: (context, state) => const DoctorProfileSetupScreen(),
+        ),
+        GoRoute(
+          path: '/doctor_home_after_setup',
+          builder: (context, state) => const DoctorHomeScreenAfterSetup(),
+        ),
+        GoRoute(
+          path: '/doctor_appointment_detail',
+          builder: (context, state) => const DoctorAppointmentDetailScreen(),
+        ),
+        GoRoute(
+          path: '/patient-login',
+          builder: (context, state) => const PatientLoginScreen(),
+        ),
+        GoRoute(
+          path: '/patient-signup',
+          builder: (context, state) => const PatientSignupScreen(),
+        ),
+        GoRoute(
+          path: '/post-booking-home',
+          builder: (context, state) => const PostBookingHomeScreen(),
+        ),
+        GoRoute(
+          path: '/appointment-list',
+          builder: (context, state) => const AppointmentListScreen(),
+        ),
+        GoRoute(
+          path: '/patient-landing',
+          builder: (context, state) =>  PatientLandingScreen(),
+        ),
+        GoRoute(
+          path: '/patient_profile',
+          builder: (context, state) => ProfileScreen(onLogout: () {  },),
+        ),
+        GoRoute(
+          path: '/settings',
+          builder: (context, state) => const SettingsScreen(),
+        ),
+        GoRoute(
+          path: '/privacy-policy',
+          builder: (context, state) => const PrivacyPolicyScreen(),
+        ),
+        GoRoute(
+          path: '/doctor-detail',
+          builder: (context, state) {
+            final doctor = state.extra as DoctorCard;
+
+            return DoctorDetailScreen(
+              name: doctor.name,
+              specialization: doctor.specialization,
+              imagePath: doctor.imagePath,
+              biography: doctor.biography,
+            );
+          },
+        ),
+
+        GoRoute(
+          path: '/book_appointment',
+          builder: (context, state) => const BookAppointmentScreen(),
+        ),
+        GoRoute(
+          path: '/success_screen',
+          builder: (context, state) => const BookingSuccessScreen(),
         ),
 
       ],
